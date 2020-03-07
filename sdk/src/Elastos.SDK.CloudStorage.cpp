4b91421f32fa121e19844c9e15af1b661cfef13f
mengxk <xiaokun.meng@qcast.cn>
2020-03-07 08:54:09 +0800

    sdk: add sdk base source.

---
+++ sdk/src/Elastos.SDK.CloudStorage.cpp
@@ -0,0 +1,46 @@
+//
+//  Elastos.SDK.CloudStorage.cpp
+//
+//  Created by mengxk on 20/03/06.
+//  Copyright © 2016 mengxk. All rights reserved.
+//
+
+
+#include <Elastos.SDK.CloudStorage.hpp>
+
+namespace elastos {
+namespace sdk {
+
+/***********************************************/
+/***** static variables initialize *************/
+/***********************************************/
+
+/***********************************************/
+/***** static function implement ***************/
+/***********************************************/
+
+
+/***********************************************/
+/***** class public function implement  ********/
+/***********************************************/
+CloudStorage::CloudStorage()
+{
+}
+
+CloudStorage::~CloudStorage()
+{
+}
+
+/***********************************************/
+/***** class protected function implement  *****/
+/***********************************************/
+
+
+/***********************************************/
+/***** class private function implement  *******/
+/***********************************************/
+
+
+} // namespace sdk
+} // namespace elastos
+
---
+++ sdk/src/Elastos.SDK.CloudStorage.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file	CloudStorage.hpp
+ * @brief	CloudStorage
+ * @details	
+ *
+ * @author	xxx
+ * @author	<xxx@xxx.com>
+ * @copyright	(c) 2012 xxx All rights reserved.
+ **/
+
+#ifndef _ELASTOS_SDK_CLOUDSTORAGE_HPP_
+#define _ELASTOS_SDK_CLOUDSTORAGE_HPP_
+
+#include <memory>
+
+namespace elastos {
+namespace sdk {
+
+class CloudStorage {
+public:
+    /*** type define ***/
+
+    /*** static function and variable ***/
+
+    /*** class function and variable ***/
+
+private:
+    /*** type define ***/
+
+    /*** static function and variable ***/
+
+    /*** class function and variable ***/
+    explicit CloudStorage();
+    virtual ~CloudStorage();
+
+
+}; // class CloudStorage
+
+} // namespace sdk
+} // namespace elastos
+
+#endif /* _ELASTOS_SDK_CLOUDSTORAGE_HPP_ */
